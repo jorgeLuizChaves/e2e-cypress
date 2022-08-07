7a650a0b00cc50998d7930fdccca61e2f8c9fdc1
jorge luiz chaves <george.luizchaves@gmail.com>
2022-08-06 23:29:26 -0300

    exercicio login

---
+++ cypress/e2e/login.cy.js
@@ -0,0 +1,11 @@
+let websiteAddress = "http://automationpractice.com/index.php";
+
+let botaoLogin = "#header > div.nav > div > div > nav > div.header_user_info > a";
+
+describe('login application', () => {
+  it('should login successfully', () => {
+    // acessar o site
+    // ir para a pagina de login
+    // fazer o login com sucesso
+  })
+})
