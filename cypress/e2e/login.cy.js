cc0c1ada305103f6458302fb6b80eae280af8c09
jorge luiz chaves <george.luizchaves@gmail.com>
2022-08-06 23:26:53 -0300

    solution login

---
+++ cypress/e2e/login.cy.js
@@ -0,0 +1,28 @@
+let websiteAddress = "http://automationpractice.com/index.php";
+
+let botaoLogin = "#header > div.nav > div > div > nav > div.header_user_info > a";
+
+describe('empty spec', () => {
+  it('passes', () => {
+    cy.visit(websiteAddress)
+    cy.get(botaoLogin).click()
+    // pega input email pelo atributo ID
+    let emailInput = cy.get('[id="email"]');
+    emailInput.type("teste1@teste.1com.br");
+
+    // pegar input pelo por outros atributos, nesse caso name
+    let passwordInput = cy.get('[name="passwd"]');
+    passwordInput.type("123123");
+
+    // pegar input button e clicar
+    let botaoLogar = cy.get('[id="SubmitLogin"]').click()
+
+    let cabecalhoLogin = cy.get('.page-heading')
+
+    // verificar se o conteúdo do text é o esperado
+    cy.get('.page-heading').should('have.text', 'My account')
+
+
+
+  })
+})
