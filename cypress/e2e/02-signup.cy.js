a85d4d9ab2213bd64154a24d8df70ece83907f33
jorge luiz chaves <george.luizchaves@gmail.com>
2022-08-12 22:13:40 -0300

    desafio exercicio 2

---
+++ cypress/e2e/02-signup.cy.js
@@ -0,0 +1,20 @@
+require('cypress-plugin-tab');
+
+let websiteAddress = "http://automationpractice.com/index.php";
+
+let botaoLogin = "#header > div.nav > div > div > nav > div.header_user_info > a";
+
+describe('signup application', () => {
+  it('should register a new user successfully', () => {
+    // acessar o site
+
+    // ir para a pagina signup
+    
+    // preencher email novo e clicar em criar nova conta
+
+    //preencher formulario e criar conta
+    
+    // validar que foi direcionado para a tela de minha conta
+
+  })
+})
